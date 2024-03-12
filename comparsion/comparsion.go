c4617e4f4f432c99012119c3a725794b15bb7a77
M. Babenko <babenoff.code@outlook.com>
2024-03-12 16:16:39 +0300

    feat(comparsion): отключение линтера misspell

--- comparsion/comparsion.go
+++ comparsion/comparsion.go
@@ -6,10 +6,12 @@ import (
 	validation "github.com/go-ozzo/ozzo-validation"
 )
 
+// Number ...
 type Number interface {
 	int | int8 | int16 | int32 | int64 | float32 | float64
 }
 
+// Gt GtreaterThe...
 func Gt[T Number](num T, msg string) validation.RuleFunc {
 	return func(value interface{}) error {
 		if value.(T) < num {
@@ -20,6 +22,7 @@ func Gt[T Number](num T, msg string) validation.RuleFunc {
 	}
 }
 
+// Lt LessThe....
 func Lt[T Number](num T, msg string) validation.RuleFunc {
 	return func(value interface{}) error {
 		if value.(T) > num {
